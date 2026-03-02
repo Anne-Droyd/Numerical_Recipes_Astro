ae6fae33a03e0427707f13ff26b7c86471f1248b
Anne-Droyd <matthewkirwan1@outlook.com>
2026-03-02 21:51:37 +0100

    trying tex output

--- hand_ins/Hand_in_1/code/Hand_in_1_code.py
+++ hand_ins/Hand_in_1/code/Hand_in_1_code.py
@@ -71,7 +71,11 @@ def question_1() -> pd.DataFrame:
         result = math_functions.poisson_distribution(lam,k)
         results.append([lam,k,result])
     results = pd.DataFrame(results,columns=['lambda','k','Poisson result'])
-    results.to_csv(os.path.join(data_dir, 'poisson.tex'))
+    results.to_csv(os.path.join(data_dir, 'poisson.csv'))
+    latex = results.to_latex(index=False,escape=False,column_format='|c|c|c|',
+                             header = True, caption = 'Poisson Results', label = 'tab:poisson')
+    with open(os.path.join(data_dir, 'poisson.tex'),'w') as f:
+        f.write(latex)
     return results
 
 def question_2() -> tuple[pd.DataFrame]:
